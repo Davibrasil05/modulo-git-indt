6047aaceca6b07cfba59ec6716cac27406398226
Patty <pafrego@gmail.com>
2025-11-05 18:50:54 -0400

    feat: mostrar_mensagem_inicial

--- desafios.py
+++ desafios.py
@@ -14,11 +14,8 @@ Boa sorte e bons commits! 🚀
 """
 
 def mostrar_mensagem_inicial():
-    """
-    Exibe uma mensagem de boas-vindas ao desafio.
-    Retorno esperado: string com a mensagem "Bem-vindo ao Desafio de Git!"
-    """
-    pass
+    return "Bem-Vindo ao Desafio de Git!"
+    
 
 def listar_comandos_git_basicos():
     """
