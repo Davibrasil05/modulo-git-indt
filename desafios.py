895535ee83353e07ffdb68b9c7296de88ab7a9c8
Patty <pafrego@gmail.com>
2025-11-05 19:55:33 -0400

    feat:gerar_relatorio_final

--- desafios.py
+++ desafios.py
@@ -47,13 +47,4 @@ def verificar_tag_valida(tag):
 
 
 def gerar_relatorio_final(funcoes_concluidas):
-    """
-    Recebe uma lista com os nomes das funções implementadas
-    e retorna uma mensagem final do desafio.
-
-    Exemplo:
-    gerar_relatorio_final(["mostrar_mensagem_inicial", "listar_comandos_git_basicos"])
-    ->
-    "Desafio concluído! 2 funções implementadas com sucesso."
-    """
-    pass
+    return f"Desafio concluído {len(funcoes_concluidas)} funções implementadas com sucesso."
